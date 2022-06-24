078521bd88833ce9c73bcebcca9fad2f435fe4bf
Alessandro De Mitri <90a.demitri@gmail.com>
2022-06-24 11:34:07 -0500

    models and associations created

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,26 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
 
-const User = require('./models/User')
+const User = require("./models/User");
+const Chefs = require("./models/Chefs");
+const HireCart = require("./models/HireCart");
+const HiredChefs = require("./models/HiredChefs");
 
 //associations could go here!
+User.hasMany(HireCart);
+HireCart.belongsTo(User);
+
+Chefs.belongsToMany(HireCart, { through: HiredChefs });
+HireCart.belongsToMany(Chefs, { through: HiredChefs });
 
 module.exports = {
   db,
   models: {
     User,
+    Chefs,
+    HireCart,
+    HiredChefs,
   },
-}
+};
+
