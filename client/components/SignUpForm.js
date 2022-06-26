fb779749231d23b22a02fdd26e5d9a4745875a92
Alessandro De Mitri <90a.demitri@gmail.com>
2022-06-25 21:28:33 -0500

    authentication components ready, but there it still doesn't work

--- client/components/SignUpForm.js
+++ client/components/SignUpForm.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {authenticate} from '../store'
+import React from 'react';
+import { connect } from 'react-redux';
+import { registration } from '../store';
 
 /**
  * COMPONENT
  */
-const AuthForm = props => {
-  const {name, displayName, handleSubmit, error} = props
+const SignUpForm = (props) => {
+  const { name, displayName, handleSubmit, error } = props;
 
   return (
     <div>
       <form onSubmit={handleSubmit} name={name}>
+        <div>
+          <label htmlFor="email">
+            <small>Email</small>
+          </label>
+          <input name="email" type="text" />
+        </div>
         <div>
           <label htmlFor="username">
             <small>Username</small>
@@ -23,32 +29,26 @@ const AuthForm = props => {
           </label>
           <input name="firstName" type="text" />
         </div>
-        {/* <div>
+        <div>
           <label htmlFor="lastName">
             <small>lastName</small>
           </label>
           <input name="lastName" type="text" />
         </div>
         <div>
-          <label htmlFor="email">
-            <small>email</small>
-          </label>
-          <input name="email" type="text" />
-        </div>
-        <div>
           <label htmlFor="password">
             <small>Password</small>
           </label>
           <input name="password" type="password" />
-        </div> */}
+        </div>
         <div>
           <button type="submit">{displayName}</button>
         </div>
         {error && error.response && <div> {error.response.data} </div>}
       </form>
     </div>
-  )
-}
+  );
+};
 
 /**
  * CONTAINER
@@ -57,33 +57,27 @@ const AuthForm = props => {
  *   function, and share the same Component. This is a good example of how we
  *   can stay DRY with interfaces that are very similar to each other!
  */
-const mapLogin = state => {
-  return {
-    name: 'login',
-    displayName: 'Login',
-    error: state.auth.error
-  }
-}
-
-const mapSignup = state => {
+const mapSignup = (state) => {
   return {
     name: 'signup',
     displayName: 'Sign Up',
-    error: state.auth.error
-  }
-}
+    error: state.auth.error,
+  };
+};
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     handleSubmit(evt) {
-      evt.preventDefault()
-      const formName = evt.target.name
-      const username = evt.target.username.value
-      const password = evt.target.password.value
-      dispatch(authenticate(username, password, formName))
-    }
-  }
-}
+      evt.preventDefault();
+      const formName = evt.target.name;
+      const email = evt.target.email.value;
+      const username = evt.target.username.value;
+      const firstName = evt.target.username.value;
+      const lastName = evt.target.username.value;
+      const password = evt.target.password.value;
+      dispatch(registration(email, username, firstName, lastName, password, formName));
+    },
+  };
+};
 
-export const Login = connect(mapLogin, mapDispatch)(AuthForm)
-export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
+export const Signup = connect(mapSignup, mapDispatch)(SignUpForm);
--- server/api/users.js
+++ server/api/users.js
@@ -6,7 +6,7 @@ module.exports = router;
 router.get("/", requireToken, isAdmin, async (req, res, next) => {
   try {
     const users = await User.findAll({
-      attributes: ["id", "username", "email", "firstName", "lastName"],
+      attributes: ["id", "username", "email", "firstName", "lastName", "password"],
     });
     res.json(users);
   } catch (err) {
